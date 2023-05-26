c8f99b43dfedb82bd1a2dc7442a125410477c5fc
dotnet-code <@dotnet-code>
2023-05-26 12:09:18 +0530

    commit from code202305260918

---
+++ cteplanand/src/app/github-configuration/github-configuration.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-configuration',
+  templateUrl: './github-configuration.component.html',
+  styleUrls: ['./github-configuration.component.css']
+})
+export class GithubConfigurationComponent implements OnInit {
+
+  errorMessage: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.errorMessage = '\'NoneType\' object has no attribute \'replace\'';
+  }
+
+}
